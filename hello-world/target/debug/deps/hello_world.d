346533ed9b9d3a074bd620aa36c358fddaded9a6
yryrgogo <yryrgogo@gmail.com>
2020-08-13 07:55:59 +0900

    :+1: add hello world sample

---
+++ hello-world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+/Users/go.horikoshi/github-ghq/github.com/yryrgogo/rust-starting/hello-world/target/debug/deps/hello_world: src/main.rs
+
+/Users/go.horikoshi/github-ghq/github.com/yryrgogo/rust-starting/hello-world/target/debug/deps/hello_world.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/go.horikoshi/github-ghq/github.com/yryrgogo/rust-starting/hello-world/target/debug/hello-world: /Users/go.horikoshi/github-ghq/github.com/yryrgogo/rust-starting/hello-world/src/main.rs

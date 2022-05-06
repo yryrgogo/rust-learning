1a57db7357fc0c6c89d6326b297a2f21eff90828
yryrgogo <yryrgogo@gmail.com>
2022-05-06 11:41:14 +0900

    :+1: trait の共有についてお勉強

---
+++ as_json/src/main.rs
@@ -0,0 +1,43 @@
+trait AsJson {
+    fn as_json(&self) -> String;
+}
+
+struct Person {
+    name: String,
+    age: u8,
+    favorite_fruit: String,
+}
+
+struct Dog {
+    name: String,
+    color: String,
+    likes_petting: bool,
+}
+
+impl AsJson for Person {
+    fn as_json(&self) -> String {
+        format!(
+            r#"{{"type": "person", "name": "{}", "age": {}, "favoriteFruit": "{}" }}"#,
+            self.name, self.age, self.favorite_fruit
+        )
+    }
+}
+
+impl AsJson for Dog {
+    fn as_json(&self) -> String {
+        format!(
+            r#"{{ "type": "dog", "name": "{}", "color": "{}", "likesPetting": {} }}"#,
+            self.name, self.color, self.likes_petting
+        )
+    }
+}
+
+fn main() {
+    let p = Person{
+        name: String::from("test"),
+        age: 10,
+        favorite_fruit: String::from("banana"),
+    };
+    println!("{}", p.as_json());
+}
+

9a2fc210a31e6743e3ee66c23aff99519330c82d
yryrgogo <yryrgogo@gmail.com>
2022-05-08 09:45:10 +0900

    :+1: Rust によるオブジェクト指向の State Pattern 実装と、Rust の強みであるコンパイラのチェックを活かした実装の比較

---
+++ oop/src/main.rs
@@ -0,0 +1,17 @@
+mod blog;
+
+use blog::Post;
+
+fn main() {
+    let mut post = Post::new();
+
+    // 今日はお昼にサラダを食べた
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
